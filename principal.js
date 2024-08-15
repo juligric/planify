02481c44adf89ef643a3a1bb365370ee1cf0cfb8
juligric <49377944@est.ort.edu.ar>
2024-08-15 11:15:26 -0300

    l

---
+++ principal.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("rect1").addEventListener("click", function() {
+        window.location.href = "http://127.0.0.1:5501/prguntamasopciones.html"; // Cambia la URL según sea necesario
+    });
+});

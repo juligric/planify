2513d844ed85dc516cb069569ee6bc82a89b127a
Bensagra <bensagra@gmail.com>
2024-11-03 23:13:01 -0300

    Refactor project structure: add new README, remove obsolete files, and update user registration logic

---
+++ juli/repository.js
@@ -0,0 +1,152 @@
+export const subMenu = [
+    [
+        "Parques",
+        "Trepar",
+        "Experiencias",
+        "Paintball",
+        "Golf",
+        "Fútbol",
+        "Acuarios",
+        "Meriendas",
+        "Ir de compras"
+    ],
+    [
+        "Bowling",
+        "Shopping",
+        "Cerámica",
+        "Cine",
+        "Yoga",
+        "Spa",
+        "Escape room",
+        "Teatro",
+        "Ir a comer"
+    ],
+    [
+        "Cerámica",
+        "Museo",
+        "Parques",
+        "Meriendas",
+        "Pool",
+        "Ir de compras",
+        "Cena",
+        "Teatro",
+        "Experiencia"
+    ],
+    [
+        "Trepar",
+        "Cine",
+        "Camas elásticas",
+        "Meriendas",
+        "Cena",
+        "Bowling",
+        "Parque de diversiones",
+        "Teatro",
+        "Escape room"
+    ],
+    [
+        "Trepar",
+        "Cine",
+        "Camas elásticas",
+        "Paintball",
+        "Cena",
+        "Bowling",
+        "Parque de diversiones",
+        "Bares",
+        "Escape room"
+    ],
+    [
+        "Experiencias",
+        "Cine",
+        "Camas elásticas",
+        "Paintball",
+        "Cena",
+        "Museo",
+        "Parque de diversiones",
+        "Bares",
+        "Fútbol"
+    ]
+];
+
+
+export const informacion = [
+    [
+        { texto: "Parque Tres de febrero", imagen: "../images/image (5).png" },
+        { texto: "Jardin japones", imagen: "../images/image.png" },
+        { texto: "Ecoparque", imagen: "../images/image (6).png" },
+        { texto: "Parque saavedra", imagen: "../images/image (2).png" },
+        { texto: "Barrancas de belgrano", imagen: "../images/image (4).png" },
+        { texto: "Jardin Botanico", imagen: "../images/image (7).png" }
+    ],
+    [
+        { texto: "Alto Parque", imagen: "../images/image (8).png" },
+        { texto: "Jardin japones", imagen: "../images/image.png" },
+        { texto: "Ecoparque", imagen: "../images/image (6).png" },
+        { texto: "Estacion Vertical", imagen: "../images/image (9).png" },
+        { texto: "Barrancas de Belgrano", imagen: "../images/image (4).png" },
+        { texto: "Jardin Botanico", imagen: "../images/image (7).png" }
+    ],
+    [
+        { texto: "Palermo Soho y Hollywood", imagen: "../images/image (10).png" },
+        { texto: "Malba", imagen: "../images/image (11).png" },
+        { texto: "Barrio chino", imagen: "../images/image (15).png" },
+        { texto: "Costanera Norte", imagen: "../images/image (12).png" },
+        { texto: "Museo Malvinas", imagen: "../images/image (13).png" },
+        { texto: "Museo Larreta", imagen: "../images/image (14).png" }
+    ],
+    [
+        { texto: "Paintball Revolution", imagen: "../images/image (16).png" },
+        { texto: "Xtreme Paintball", imagen: "../images/image (17).png" },
+        { texto: "Paintball Zona Norte", imagen: "../images/image (18).png" },
+        { texto: "Urban Paintball", imagen: "../images/image (19).png" },
+        { texto: "Warzone Paintball", imagen: "../images/image (20).png" },
+        { texto: "Action live Paintball", imagen: "../images/image (21).png" }
+    ],
+    [
+        { texto: "Paintball Revolution", imagen: "../images/image (16).png" },
+        { texto: "Xtreme Paintball", imagen: "../images/image (17).png" },
+        { texto: "Paintball Zona Norte", imagen: "../images/image (18).png" },
+        { texto: "Urban Paintball", imagen: "../images/image (19).png" },
+        { texto: "Warzone Paintball", imagen: "../images/image (20).png" },
+        { texto: "Action live Paintball", imagen: "../images/image (21).png" }
+    ],
+    [
+        { texto: "Paintball Revolution", imagen: "../images/image (16).png" },
+        { texto: "Xtreme Paintball", imagen: "../images/image (17).png" },
+        { texto: "Paintball Zona Norte", imagen: "../images/image (18).png" },
+        { texto: "Urban Paintball", imagen: "../images/image (19).png" },
+        { texto: "Warzone Paintball", imagen: "../images/image (20).png" },
+        { texto: "Action live Paintball", imagen: "../images/image (21).png" }
+    ],
+    [
+        { texto: "Paintball Revolution", imagen: "../images/image (16).png" },
+        { texto: "Xtreme Paintball", imagen: "../images/image (17).png" },
+        { texto: "Paintball Zona Norte", imagen: "../images/image (18).png" },
+        { texto: "Urban Paintball", imagen: "../images/image (19).png" },
+        { texto: "Warzone Paintball", imagen: "../images/image (20).png" },
+        { texto: "Action live Paintball", imagen: "../images/image (21).png" }
+    ],
+    [
+        { texto: "Patio de los lecheros", imagen: "../images/meriendas 1.png" },
+        { texto: "Lagos de Regatas", imagen: "../images/meriendas 2.png" },
+        { texto: "El Rosedal Café", imagen: "../images/meriendas 3.png" },
+        { texto: "Club de Pescadores", imagen: "../images/meriendas 4.png" },
+        { texto: "Casa Cavia", imagen: "../images/meriendas 5.png" },
+        { texto: "Action live Paintball", imagen: "../images/meriendas 6.png" }
+    ],
+    [
+        { texto: "Shopping Dot Baires", imagen: "../images/dot.png" },
+        { texto: "Distrito Arcos", imagen: "../images/arcos.png" },
+        { texto: "Alto Palermo Shopping", imagen: "../images/palermo.png" },
+        { texto: "Patio Bullrich", imagen: "../images/patio.png" },
+        { texto: "Alcorta Shopping", imagen: "../images/alcorta.png" },
+        { texto: "Galerias del Pacifico", imagen: "../images/galeria.png" }
+    ],
+    [
+        { texto: "Paloko Bowling", imagen: "../images/bowling6.png" },
+        { texto: "Bowling Snack", imagen: "../images/bowling1.png" },
+        { texto: "Brasov Bowling - Pilar", imagen: "../images/bowling2.png" },
+        { texto: "Bowling Palermo", imagen: "../images/bowling3.png" },
+        { texto: "Bowling Paloko - DOT", imagen: "../images/bowling4.png" },
+        { texto: "Galerias del Pacifico", imagen: "../images/bowling5.png" }
+    ]
+];
